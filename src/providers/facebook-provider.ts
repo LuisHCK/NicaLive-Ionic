220d452569b5da43f8b01fac2a7a113cdf68272e
luishck <luisjcenteno@live.com>
2017-05-29 20:50:05 -0600

    share things

--- src/providers/facebook-provider.ts
+++ src/providers/facebook-provider.ts
@@ -4,3 +4,4 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/timeout';
 
 "https://ionicthemes.com/tutorials/about/ionic2-facebook-login"
+"search?q=marzosbar.lounge&type=page&fields=name,description_html,location,category,phone,picture,cover"
