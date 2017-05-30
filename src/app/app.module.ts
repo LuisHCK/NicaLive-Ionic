2e8892c4fd2d3057a85b443e8912bb39f16dc51b
luishck <luisjcenteno@live.com>
2017-05-29 20:36:54 -0600

    Login template and basic views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
+import { SearchPage } from '../pages/search/search';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from './../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,10 +15,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    SearchPage,
     ContactPage,
     HomePage,
-    TabsPage
+    LoginPage,
+    TabsPage,
   ],
   imports: [
     BrowserModule,
@@ -26,9 +28,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    SearchPage,
     ContactPage,
     HomePage,
+    LoginPage,
     TabsPage
   ],
   providers: [
---
+++ src/providers/facebook-provider.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@angular/core';
+import { Http, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/timeout';
+
+"https://ionicthemes.com/tutorials/about/ionic2-facebook-login"
